801e1889fafc10a3cc81204ca8afd30ada9bda23
Rathiesh <rathiesh2002@gmail.com>
2024-05-29 09:41:20 +0530

    adding files

---
+++ components/BackgroundCircles.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+type Props = {}
+
+function BackgroundCircles({}: Props) {
+  return (
+    <div>BackgroundCircles</div>
+  )
+}
+
+export default BackgroundCircles
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
       <Head>
         <title>Rathiesh Portfolio</title>
       </Head>
       <Header/>
+      <section id='hero'>
+        <Hero/>
+      </section>
     </div>  
   )
 }
