afd30ab7f24c4da2199fdc70b2ea2f8275161fc5
Rathiesh <rathiesh2002@gmail.com>
2024-05-30 10:35:49 +0530

    feat: Add WorkExperience and Skills components
    This commit adds the WorkExperience and Skills components to the project. The WorkExperience component displays a section with experience cards, and the Skills component displays a grid of skills with hover effects. These components enhance the portfolio website by providing more information about the user's experience and skills.

---
+++ components/About.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { motion } from "framer-motion";
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
+        viewport={{ once: true }}
+        className="-mb-20 mt-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
+        src="https://editors.dexerto.com/wp-content/uploads/2024/04/08/one-piece-luffy-gear-5.jpeg"
+        alt=""
+      />
+
+      <div className="space-y-10 mt-20 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+          background
+        </h4>
+       
+      </div>
+    </motion.div>
+  );
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,28 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
+import WorkExperience from '../components/WorkExperience'
+import Skills from '../components/Skills'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll z-0'>
       <Head>
         <title>Rathiesh Portfolio</title>
       </Head>
       <Header/>
-      <section id='hero'>
+      <section id='hero' className='snap-start'>
         <Hero/>
+      </section>
+      <section id='about' className='snap-center'>
+        <About/>
+      </section>
+      <section id='experience' className='snap-center'>
+        <WorkExperience/>
+      </section>
+      <section id='skills' className='snap-start'>
+        <Skills/>
       </section>
     </div>  
   )
